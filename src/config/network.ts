0ded00add4b48d6b4d6397faeae27737a4e98659
Sravani <sravani@echortech.com>
2025-10-07 10:57:02 +0530

    chore: update devnet configuration
    - Add separate FT_DENOM_CREATOR for devnet: bafybmie3sriw6nnro5nmhgwembm7p4gynfvgkpghjoa23wkarqecvhvudi
    - Update devnet faucet URL to use HTTPS instead of HTTP

--- src/config/network.ts
+++ src/config/network.ts
@@ -156,7 +156,11 @@ const CONTRACT_ADDRESSES: ContractAddresses = {
     ? "QmS5DogBfk96voS54hhE4KemToGRWgGC6Fbk5cZboTNh3m"
     : "QmVScNzdPRuN2r7DYwcnr3PVFB4s6TPWv5o9iVWsuqyPeW",
   
-  FT_DENOM_CREATOR: "bafybmifzar4metqgkm4ivtnvabmiouyi32y2x2ikpi5h4tflrfug2ghi5q",
+  FT_DENOM_CREATOR: isMainnet 
+    ? "bafybmifzar4metqgkm4ivtnvabmiouyi32y2x2ikpi5h4tflrfug2ghi5q" 
+    : isTestnet 
+    ? "bafybmifzar4metqgkm4ivtnvabmiouyi32y2x2ikpi5h4tflrfug2ghi5q"
+    : "bafybmie3sriw6nnro5nmhgwembm7p4gynfvgkpghjoa23wkarqecvhvudi",
   
   FT_DENOM: "TRIE",
   
@@ -182,7 +186,7 @@ const API_ENDPOINTS: ApiEndpoints = {
   
   faucet: isMainnet ? "" : 
           isTestnet ? "https://trie-faucet-api.trie.network" :
-          "http://dev-faucet-api.trie.network:8443",
+          "https://dev-faucet-api.trie.network:8443",
   
   metrics: isMainnet 
     ? "https://mainnet.xellwallet.com:8449/" 
